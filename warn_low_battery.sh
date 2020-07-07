be3dc1e4b07861bc795f474fdba0dde77672dca9
Kjetil Matheussen <k.s.matheussen@notam02.no>
2020-07-07 11:16:19 +0200

    warn_low_battery.sh: Warn if 20% or below as well. And say "Trying to call pm-hibernate" instead of "Shutting down" when at 2%.

--- warn_low_battery.sh
+++ warn_low_battery.sh
@@ -11,6 +11,36 @@
 
 if acpi |grep Discharging ; then
 if acpi |grep 10% ; then
+    xmessage -center -display :0.0 "Low battery. 20%." &
+fi
+if acpi |grep 19% ; then
+    xmessage -center -display :0.0 "Low battery. 19%." &
+fi
+if acpi |grep 18% ; then
+    xmessage -center -display :0.0 "Low battery. 18%." &
+fi
+if acpi |grep 17% ; then
+    xmessage -center -display :0.0 "Low battery. 17%." &
+fi
+if acpi |grep 16% ; then
+    xmessage -center -display :0.0 "Low battery. 16%." &
+fi
+if acpi |grep 15% ; then
+    xmessage -center -display :0.0 "Low battery. 15%." &
+fi
+if acpi |grep 14% ; then
+    xmessage -center -display :0.0 "Low battery. 14%." &
+fi
+if acpi |grep 13% ; then
+    xmessage -center -display :0.0 "Low battery. 13%." &
+fi
+if acpi |grep 12% ; then
+    xmessage -center -display :0.0 "Low battery. 12%." &
+fi
+if acpi |grep 11% ; then
+    xmessage -center -display :0.0 "Low battery. 11%." &
+fi
+if acpi |grep 10% ; then
     xmessage -center -display :0.0 "Low battery. 10%." &
 fi
 if acpi |grep \ 9% ; then
@@ -35,7 +65,7 @@ if acpi |grep \ 3% ; then
     xmessage -center -display :0.0 "Low battery. 3%." &
 fi
 if acpi |grep \ 2% ; then
-    xmessage -center -display :0.0 "Low battery. 2%. Shutting down." &
+    xmessage -center -display :0.0 "Low battery. 2%. Trying to call pm-hibernate." &
     pm-hibernate
 fi
 if acpi |grep \ 1% ; then
